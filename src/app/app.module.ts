5f89c2ad571c8df68df296f2da50e5fa686508c8
shalin <shalin.rono@student.moringaschool.com>
2021-08-29 20:34:50 +0300

    add content in datecount pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { SearchComponent } from './search/search.component';
+import { UserComponent } from './user/user.component';
 import { DateCountPipe } from './date-count.pipe';
+import { HttpClientModule } from '@angular/common/http';
+import { SearchService } from './search.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
     SearchComponent,
+    UserComponent,
     DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
